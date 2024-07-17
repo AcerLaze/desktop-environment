b28eedba63a7c51891af85b0aab96be1d04c149d
AcerLaze <49096134+AcerLaze@users.noreply.github.com>
2024-07-17 14:50:58 +0800

    skhd config

---
+++ home/.config/skhd/floating_terminal.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+focused_app="$(yabai -m query --windows --window | jq -re '.app')"
+focused_id="$(yabai -m query --windows --window | jq '.id')"
+terminal="kitty"
+
+if [[ $focused_app = ${terminal} ]]; then
+	osascript -e "tell application \"System Events\" to set visible of first process ¬" \
+          -e "whose name is \"${terminal}\" to false"	
+else
+	osascript -e "tell application \"System Events\" to set visible of first process ¬" \
+		-e "whose name is \"${terminal}\" to true"
+
+	osascript -e "tell application \"System Events\" to tell process \"${terminal}\"" \
+            -e 'set frontmost to true' \
+            -e 'if windows is not {} then perform action "AXRaise" of item 1 of windows' \
+            -e 'end tell'
+fi
+
