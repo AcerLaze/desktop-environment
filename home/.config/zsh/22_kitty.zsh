4da380f9e80b4489cf240dd343ad443d36b5a60f
AcerLaze <49096134+AcerLaze@users.noreply.github.com>
2025-01-28 13:54:57 +0700

    chore: update settings.json

--- home/.config/zsh/22_kitty.zsh
+++ home/.config/zsh/22_kitty.zsh
@@ -1,5 +1,5 @@
 if [[ $TERM = xterm-kitty ]]; then
-  alias icat=kitty +kitten icat
-  alias diff=kitty +kitten diff
-  alias ssh=kitty +kitten ssh
+  alias icat="kitty +kitten icat"
+  alias diff="kitty +kitten diff"
+  alias ssh="kitty +kitten ssh"
 fi
